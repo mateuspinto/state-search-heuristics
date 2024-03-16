cf5dc435ee00638e7ca4655d19e27042a26134f6
Mateus Pinto da Silva <mateusps10@outlook.com>
2024-03-16 16:46:17 -0300

    fix: formatting with ruff

--- search.py
+++ search.py
@@ -1,12 +1,13 @@
 import heapq
 from math import sqrt
 
-WALL = 'X'
-START_STATE = 'S'
-GOAL_STATE  = 'G'
+WALL = "X"
+START_STATE = "S"
+GOAL_STATE = "G"
 
-def plan(map, algorithm='bfs', heuristic=None):
-    """ Loads a level, searches for a path between the given waypoints, and displays the result.
+
+def plan(map, algorithm="bfs", heuristic=None):
+    """Loads a level, searches for a path between the given waypoints, and displays the result.
 
     Args:
         filename: The name of the text file containing the level.
@@ -22,40 +23,45 @@ def plan(map, algorithm='bfs', heuristic=None):
     level = parse_level(map)
 
     # Retrieve the source and destination coordinates from the level.
-    start = level['start']
-    goal = level['goal']
+    start = level["start"]
+    goal = level["goal"]
 
     # Search for and display the path from src to dst.
     path = []
     visited = {}
 
-    if algorithm == 'bfs':
+    if algorithm == "bfs":
         path, visited = bfs(start, goal, level, transition_model)
-    elif algorithm == 'dfs':
+    elif algorithm == "dfs":
         path, visited = dfs(start, goal, level, transition_model)
-    elif algorithm == 'ucs':
+    elif algorithm == "ucs":
         path, visited = ucs(start, goal, level, transition_model)
-    elif algorithm == 'greedy':
-        if heuristic == 'euclidian':
-            path, visited = greedy_best_first(start, goal, level, transition_model, h_euclidian)
-        elif heuristic == 'manhattan':
-            path, visited = greedy_best_first(start, goal, level, transition_model, h_manhattan)
-    elif algorithm == 'astar':
-        if heuristic == 'euclidian':
+    elif algorithm == "greedy":
+        if heuristic == "euclidian":
+            path, visited = greedy_best_first(
+                start, goal, level, transition_model, h_euclidian
+            )
+        elif heuristic == "manhattan":
+            path, visited = greedy_best_first(
+                start, goal, level, transition_model, h_manhattan
+            )
+    elif algorithm == "astar":
+        if heuristic == "euclidian":
             path, visited = a_star(start, goal, level, transition_model, h_euclidian)
-        elif heuristic == 'manhattan':
+        elif heuristic == "manhattan":
             path, visited = a_star(start, goal, level, transition_model, h_manhattan)
 
     return path, path_cost(path, level), visited
 
+
 def parse_level(map):
-    """ Parses a level from a string.
+    """Parses a level from a string.
 
     Args:
         level_str: A string containing a level.
 
     Returns:
-        The parsed level (dict) containing the locations of walls (set), the locations of spaces 
+        The parsed level (dict) containing the locations of walls (set), the locations of spaces
         (dict), and a mapping of locations to waypoints (dict).
     """
     start = None
@@ -63,27 +69,28 @@ def parse_level(map):
     walls = set()
     spaces = {}
 
-    for j, line in enumerate(map.split('\n')):
+    for j, line in enumerate(map.split("\n")):
         for i, char in enumerate(line):
-            if char == '\n':
+            if char == "\n":
                 continue
             elif char == WALL:
                 walls.add((i, j))
             elif char == START_STATE:
                 start = (i, j)
-                spaces[(i, j)] = 1.
+                spaces[(i, j)] = 1.0
             elif char == GOAL_STATE:
-                goal = (i, j) 
-                spaces[(i, j)] = 1.
+                goal = (i, j)
+                spaces[(i, j)] = 1.0
             elif char.isnumeric():
                 spaces[(i, j)] = float(char)
 
-    level = {'walls': walls, 'spaces': spaces, 'start': start, 'goal': goal}
+    level = {"walls": walls, "spaces": spaces, "start": start, "goal": goal}
 
     return level
 
+
 def path_cost(path, level):
-    """ Returns the cost of the given path.
+    """Returns the cost of the given path.
 
     Args:
         path: A list of cells from the source to the goal.
@@ -94,18 +101,24 @@ def path_cost(path, level):
     """
     cost = 0
     for i in range(len(path) - 1):
-        cost += cost_function(level, path[i], path[i + 1], 
-                              level['spaces'][path[i]], 
-                              level['spaces'][path[i + 1]])
+        cost += cost_function(
+            level,
+            path[i],
+            path[i + 1],
+            level["spaces"][path[i]],
+            level["spaces"][path[i + 1]],
+        )
 
     return cost
+
 
 # =============================
 # Transition Model
 # =============================
 
+
 def cost_function(level, state1, state2, cost1, cost2):
-    """ Returns the cost of the edge joining state1 and state2.
+    """Returns the cost of the edge joining state1 and state2.
 
     Args:
         state1: A source location.
@@ -121,15 +134,16 @@ def cost_function(level, state1, state2, cost1, cost2):
 
     return 0
 
+
 def transition_model(level, state1):
-    """ Provides a list of adjacent states and their respective costs from the given state.
+    """Provides a list of adjacent states and their respective costs from the given state.
 
     Args:
         level: A loaded level, containing walls, spaces, and waypoints.
         state: A target location.
 
     Returns:
-        A list of tuples containing an adjacent sates's coordinates and the cost of 
+        A list of tuples containing an adjacent sates's coordinates and the cost of
         the edge joining it and the originating state.
 
         E.g. from (0,0):
@@ -146,12 +160,14 @@ def transition_model(level, state1):
 
     return adj_states.items()
 
+
 # =============================
 # Uninformed Search Algorithms
 # =============================
 
+
 def bfs(s, g, level, adj):
-    """ Searches for a path from the source to the goal using the Breadth-First Search algorithm.
+    """Searches for a path from the source to the goal using the Breadth-First Search algorithm.
 
     Args:
         s: The source location.
@@ -160,7 +176,7 @@ def bfs(s, g, level, adj):
         adj: A function that returns the adjacent cells and their respective costs from the given cell.
 
     Returns:
-        A list of tuples containing cells from the source to the goal, and a dictionary 
+        A list of tuples containing cells from the source to the goal, and a dictionary
         containing the visited cells and their respective parent cells.
     """
     visited = {s: None}
@@ -171,14 +187,15 @@ def bfs(s, g, level, adj):
 
     return [], visited
 
+
 def dfs(s, g, level, adj):
-    """ Searches for a path from the source to the goal using the Depth-First Search algorithm.
+    """Searches for a path from the source to the goal using the Depth-First Search algorithm.
     Args:
         s: The source location.
         g: The goal location.
         level: The level containing the locations of walls, spaces, and waypoints.
         adj: A function that returns the adjacent cells and their respective costs from the given cell.
-    
+
     Returns:
         A list of tuples containing cells from the source to the goal, and a dictionary containing the visited cells and their respective parent cells.
     """
@@ -190,8 +207,9 @@ def dfs(s, g, level, adj):
 
     return [], visited
 
+
 def ucs(s, g, level, adj):
-    """ Searches for a path from the source to the goal using the Uniform-Cost Search algorithm.
+    """Searches for a path from the source to the goal using the Uniform-Cost Search algorithm.
 
     Args:
         s: The source location.
@@ -210,12 +228,13 @@ def ucs(s, g, level, adj):
 
     return [], visited
 
+
 # ======================================
 # Informed (Heuristic) Search Algorithms
 # ======================================
 def greedy_best_first(s, g, level, adj, h):
-    """ Searches for a path from the source to the goal using the Greedy Best-First Search algorithm.
-    
+    """Searches for a path from the source to the goal using the Greedy Best-First Search algorithm.
+
     Args:
         s: The source location.
         g: The goal location.
@@ -234,8 +253,9 @@ def greedy_best_first(s, g, level, adj, h):
 
     return [], visited
 
+
 def a_star(s, g, level, adj, h):
-    """ Searches for a path from the source to the goal using the A* algorithm.
+    """Searches for a path from the source to the goal using the A* algorithm.
 
     Args:
         s: The source location.
@@ -243,7 +263,7 @@ def a_star(s, g, level, adj, h):
         level: The level containing the locations of walls, spaces, and waypoints.
         adj: A function that returns the adjacent cells and their respective costs from the given cell.
         h: A heuristic function that estimates the cost from the current cell to the goal.
-    
+
     Returns:
         A list of tuples containing cells from the source to the goal, and a dictionary containing the visited cells and their respective parent cells.
     """
@@ -255,18 +275,19 @@ def a_star(s, g, level, adj, h):
 
     return [], visited
 
+
 # ======================================
 # Heuristic functions
 # ======================================
 def h_euclidian(s, g):
-    """ Estimates the cost from the current cell to the goal using the Euclidian distance.
+    """Estimates the cost from the current cell to the goal using the Euclidian distance.
 
     Args:
         s: The current location.
         g: The goal location.
-    
+
     Returns:
-        The estimated cost from the current cell to the goal.    
+        The estimated cost from the current cell to the goal.
     """
 
     ################################
@@ -275,13 +296,14 @@ def h_euclidian(s, g):
 
     return 0
 
+
 def h_manhattan(s, g):
-    """ Estimates the cost from the current cell to the goal using the Manhattan distance.
-    
+    """Estimates the cost from the current cell to the goal using the Manhattan distance.
+
     Args:
         s: The current location.
         g: The goal location.
-    
+
     Returns:
         The estimated cost from the current cell to the goal.
     """
--- server.py
+++ server.py
@@ -5,26 +5,28 @@ from search import plan
 app = Flask(__name__)
 
 # Directory containing the text files
-maps_directory = 'maps'
+maps_directory = "maps"
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/get_maps', methods=['GET'])
+
+@app.route("/get_maps", methods=["GET"])
 def get_maps():
     maps = {}
     try:
         # Check if the directory exists
         if os.path.exists(maps_directory):
             # Get a list of all files with a .txt extension
-            txt_files = [f for f in os.listdir(maps_directory) if f.endswith('.txt')]
+            txt_files = [f for f in os.listdir(maps_directory) if f.endswith(".txt")]
 
             for txt_file in txt_files:
                 file_path = os.path.join(maps_directory, txt_file)
 
                 # Read the content of the file
-                with open(file_path, 'r') as file:
+                with open(file_path, "r") as file:
                     content = file.read()
 
                 # Extract the filename without extension
@@ -33,24 +35,27 @@ def get_maps():
                 # Add the content to the dictionary with filename as key
                 maps[filename] = content
 
-            response = jsonify({'result': 'success', 'maps': maps})
+            response = jsonify({"result": "success", "maps": maps})
             response.headers.add("Access-Control-Allow-Origin", "*")
 
             return response
         else:
-            return jsonify({'result': 'error', 'error_details': 'Directory not found'}), 404
-        
+            return jsonify(
+                {"result": "error", "error_details": "Directory not found"}
+            ), 404
+
     except Exception as e:
-        error_message = {'result': 'error', 'error_details': str(e)}
+        error_message = {"result": "error", "error_details": str(e)}
         return jsonify(error_message), 500
-    
-@app.route('/start_search', methods=['GET'])
+
+
+@app.route("/start_search", methods=["GET"])
 def start_search():
     try:
         # Get map_name and algorithm_name from query parameters
-        map = request.args.get('map')
-        alg = request.args.get('alg')
-        heuristic = request.args.get('heuristic')
+        map = request.args.get("map")
+        alg = request.args.get("alg")
+        heuristic = request.args.get("heuristic")
 
         # Plan the path
         path, path_cost, visited = plan(map, alg, heuristic)
@@ -63,34 +68,35 @@ def start_search():
 
         # Perform search algorithm logic here with map_name and algorithm_name
         # For demonstration purposes, just returning a simple response
-        response = jsonify({'result': 'success', 'path': path, 'visited': visited})
+        response = jsonify({"result": "success", "path": path, "visited": visited})
         response.headers.add("Access-Control-Allow-Origin", "*")
 
         return response
 
     except Exception as e:
-        return jsonify({'result': 'error', 'error_details': str(e)}), 500
+        return jsonify({"result": "error", "error_details": str(e)}), 500
 
-@app.route('/save_map', methods=['GET'])
+
+@app.route("/save_map", methods=["GET"])
 def save_map():
     try:
         # Get filename and content from query parameters
-        map_name = request.args.get('map_name')
-        map_data = request.args.get('map')
+        map_name = request.args.get("map_name")
+        map_data = request.args.get("map")
 
         # Write the content to a file
-        with open(f'{maps_directory}/{map_name}.txt', 'w') as file:
+        with open(f"{maps_directory}/{map_name}.txt", "w") as file:
             file.write(map_data)
 
-        response = jsonify({'result': 'success'})
+        response = jsonify({"result": "success"})
         response.headers.add("Access-Control-Allow-Origin", "*")
 
         return response
 
     except Exception as e:
-        return jsonify({'result': 'error', 'error_details': str(e)}), 500
+        return jsonify({"result": "error", "error_details": str(e)}), 500
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Use the SSL context for HTTPS
     app.run(host="0.0.0.0", port="5001", debug=True)
